0973d8e012577dac791ad72cf8b8ff7cf19c372c
irinaliz <dlqhals3@naver.com>
2022-03-07 07:54:43 +0900

    3일차

--- dist/todo.js
+++ dist/todo.js
@@ -1,8 +1,35 @@
-var me3 = {
-    name: 'yc',
-    age: 34,
-};
-var me4 = {
-    name: 'yc',
-    age: 31,
+window.onload = function () {
+    var byid = function (id) { return document.getElementById(id); };
+    var listBox = byid("list");
+    var addButton = byid("add");
+    var saveButton = byid("saveButton");
+    var closeButton = byid("close");
+    var addTodo = function () {
+        var textElement = byid("ControlInput1");
+        var text = textElement.value;
+        var todoItem = {
+            div: document.createElement('div'),
+            input: document.createElement('input'),
+            label: document.createElement('label'),
+            text: "클릭해서 내용을 바꿔주세요!"
+        };
+        var itemId = "item" + document.getElementById("list").children.length;
+        todoItem.input.className = "form-check-input";
+        todoItem.input.type = "checkbox";
+        todoItem.input.id = itemId;
+        todoItem.label.className = "form-check-label";
+        todoItem.label.htmlFor = itemId;
+        todoItem.label.innerText = text;
+        todoItem.div.className = "form-check";
+        todoItem.div.append(todoItem.input);
+        todoItem.div.append(todoItem.label);
+        listBox.append(todoItem.div);
+        closeButton.click();
+    };
+    saveButton.addEventListener("click", addTodo);
+    addButton.addEventListener("click", function () {
+        setTimeout(function () {
+            byid("ControlInput1").focus();
+        }, 500);
+    });
 };
--- src/todo.ts
+++ src/todo.ts
@@ -1,23 +1,53 @@
+window.onload = () =>{
+    const byid = (id:string) =>{return document.getElementById(id)}
+    const listBox:HTMLElement = byid("list");
+    const addButton:HTMLElement = byid("add");
+    const saveButton:HTMLElement = byid("saveButton");
+    const closeButton:HTMLElement = <HTMLButtonElement> byid("close");
 
-interface PeopleInterface2 {
-    name: string
-    age: number
+    interface TodoItem {
+        div:HTMLDivElement,
+        input:HTMLInputElement,
+        label:HTMLLabelElement,
+        text:string
+    }
+
+    const addTodo = () => {
+        const textElement = <HTMLInputElement> byid("ControlInput1");
+        const text:string = textElement.value;
+
+        let todoItem:TodoItem = {
+            div:document.createElement('div') as HTMLDivElement , 
+            input:document.createElement('input') as HTMLInputElement,
+            label:document.createElement('label') as HTMLLabelElement, 
+            text: "클릭해서 내용을 바꿔주세요!"  
+        };
+        
+        let itemId = "item"+document.getElementById("list").children.length;
+
+        todoItem.input.className = "form-check-input";
+        todoItem.input.type = "checkbox";
+        todoItem.input.id = itemId;
+
+        todoItem.label.className = "form-check-label";
+        todoItem.label.htmlFor = itemId;
+        todoItem.label.innerText = text;
+
+        todoItem.div.className = "form-check";
+        todoItem.div.append(todoItem.input);
+        todoItem.div.append(todoItem.label);
+        listBox.append(todoItem.div);
+
+        closeButton.click();
+
+    }
+
+
+    
+    saveButton.addEventListener("click",addTodo);
+    addButton.addEventListener("click", ()=>{
+        setTimeout(()=>{
+            byid("ControlInput1").focus();
+        },500);
+    })
 }
-
-const me3: PeopleInterface2 = {
-    name: 'yc',
-    age: 34,
-}
-
-type PeopleType2 = {
-    name: string
-    age: number
-}
-
-const me4: PeopleType2 = {
-    name: 'yc',
-    age: 31,
-}
-
-
-
